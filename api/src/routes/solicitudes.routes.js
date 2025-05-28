8b9e39ffdea750af0ffc571ac468292ce81697e7
Diego Trujillo <diegotrujillo0210@gmail.com>
2025-05-28 01:06:07 -0500

    feat: agregar ruta para listar solicitudes en el enrutador de solicitudes

--- api/src/routes/solicitudes.routes.js
+++ api/src/routes/solicitudes.routes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { createRequest } = require('../controllers/solicitudes.controllers');
+const { createRequest, listRequests } = require('../controllers/solicitudes.controllers');
 
 router.post('/', createRequest);
+router.get('/', listRequests);
 
 module.exports = router;
