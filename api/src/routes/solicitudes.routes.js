0482c81e7c90923b4b2c2c0ba5ddadc79b5f9698
Diego Trujillo <diegotrujillo0210@gmail.com>
2025-05-28 09:06:36 -0500

    feat: agregar ruta para eliminar solicitudes en el enrutador de solicitudes

--- api/src/routes/solicitudes.routes.js
+++ api/src/routes/solicitudes.routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { createRequest, getRequests, updateRequest } = require('../controllers/solicitudes.controllers');
+const { createRequest, getRequests, updateRequest, deleteRequest } = require('../controllers/solicitudes.controllers');
 
 router.post('/', createRequest);
 router.get('/', getRequests);
 router.put('/:id', updateRequest);
+router.delete('/:id', deleteRequest);
+
 
 module.exports = router;
