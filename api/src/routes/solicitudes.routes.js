ffa242f8db2b701e720802a16efba96bf552f9f8
Diego Trujillo <diegotrujillo0210@gmail.com>
2025-05-29 23:29:45 -0500

    feat: agregar ruta para obtener solicitud por ID en el enrutador de solicitudes

--- api/src/routes/solicitudes.routes.js
+++ api/src/routes/solicitudes.routes.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { createRequest, getRequests, updateRequest, deleteRequest } = require('../controllers/solicitudes.controllers');
+const { createRequest, getRequests, updateRequest, deleteRequest, getRequestById } = require('../controllers/solicitudes.controllers');
 const { addService } = require('../controllers/servicios.controllers');
 
 router.post('/', createRequest);
 router.get('/', getRequests);
 router.put('/:id', updateRequest);
 router.delete('/:id', deleteRequest);
+router.get('/:id', getRequestById);
+
 
 // servicios
 
