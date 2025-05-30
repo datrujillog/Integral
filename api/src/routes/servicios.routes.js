42691b93d112f21f84af0dbcc2cdc053df6b163e
Diego Trujillo <diegotrujillo0210@gmail.com>
2025-05-29 23:35:29 -0500

    feat: agregar ruta para eliminar servicios en el enrutador de servicios

--- api/src/routes/servicios.routes.js
+++ api/src/routes/servicios.routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { addService, updateService } = require('../controllers/servicios.controllers');
+const { addService, updateService, removeService } = require('../controllers/servicios.controllers');
 
 router.post('/solicitudes/:id/servicios', addService);
+router.delete('/:id', removeService);
+
 
 router.put('/:id', updateService);
 
